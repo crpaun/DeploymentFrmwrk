dae654181a48217fb447da77d849fd4705fab833
Cristian Paun <cristian.paun@3pillarglobal.com>
2013-09-11 12:57:06 +0300

    minor refactoring, added reset db to deploy task

--- threepillar/daf/fabfile.py
+++ threepillar/daf/fabfile.py
@@ -14,62 +14,71 @@ env.password = util.read_config_map('authentication')['ssh_password']
 
     
 #===============================================================================
-# main desploy task:
-#            stop app server
-#            cleanup temp app server data
-#            fetch deployment artifacts
-#            start app server    
+# performs the following:
+#            app_server_shutdown()
+#            reset_db()
+#            app_server_cleanup()
+#            fetch_deployment_from_repository(deployment_type,version)
+#            app_server_startup()    
+#            check_deployment_status()
 #===============================================================================
-def deploy(deployment_type,version):
+def deploy(deployment_type,version):    
     app_server_shutdown()
+    reset_db()
     app_server_cleanup()
     fetch_deployment_from_repository(deployment_type,version)
     app_server_startup()
     check_deployment_status()
-    
 #===============================================================================
-# Start/stop/cleanup app server
+# Starts tomcat on deployment server
 #===============================================================================
 def app_server_startup():
     """
     startup tomcat instance.
     """
     util.tomcat_startup(verbose=False)
-    
+#===============================================================================
+# Stops tomcat on deployment server
+#===============================================================================
 def app_server_shutdown():
     """
     shut down tomcat instance.
     """
     util.tomcat_shutdown(verbose=False)
-
+#===============================================================================
+# Remove tomcat working items
+#===============================================================================
 def app_server_cleanup():
     """
     startup tomcat instance.
     """
     util.tomcat_cleanup(verbose=False)
-    
-
 #===============================================================================
-# artifact_type can either be SNAPSHOT or RELEASE
+# copy .war from nexus to the deployment server 
+#     deployment_type can either be SNAPSHOT or RELEASE
+#     version must match the one in maven;s pom.xml
 #===============================================================================
 def fetch_deployment_from_repository(deployment_type,version):
     util.fetch_deployment_from_nexus(deployment_type,version)
-
 #===============================================================================
-# check if application is running 
+# check if application is up and running 
 #===============================================================================
 def check_deployment_status():
     util.check_deployment_status(env.hosts[0])
     
 #===============================================================================
-# db specific - run sql DDL and seed 
+# executes all sql scripts located inside db_scripts_tmp  
 #===============================================================================
-def setup_db():
+def reset_db():
     util.setup_mysql_db()
 
 #===============================================================================
 # seed jenkins job from xml config file
 #===============================================================================
+
+
+
+
 def seed_jenkins_jobs():
     util.seed_jenkins_jobs()
 
